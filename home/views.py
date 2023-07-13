5c9c587a6d03dde13c53c8fad853b093d6ca6dd1
fitabigail <abidinu35@gmail.com>
2023-07-13 16:39:37 +0000

    Fix home app issue

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Home page view here.
+
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
