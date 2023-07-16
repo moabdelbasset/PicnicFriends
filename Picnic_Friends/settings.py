a7138c679dc9c94a66f0f0c8fe491472e53c1e45
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-07-16 20:01:35 +0000

    Update settings for deployment

--- Picnic_Friends/settings.py
+++ Picnic_Friends/settings.py
@@ -33,6 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = [os.environ.get("HOST"), '127.0.0.1', 'localhost', 'picnicfriends-b3d57c6873d2.herokuapp.com']
 
 # Application definition
