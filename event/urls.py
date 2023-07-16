26fef51f7992351f30399a88bb348bbddbb8e09d
fitabigail <abidinu35@gmail.com>
2023-07-16 11:05:22 +0000

    Add search function by name and description.

--- event/urls.py
+++ event/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [  
+urlpatterns = [ 
+    path('events', views.all_events, name='all_events'), 
     path('add_event', views.add_event, name='add_event'),
-
+    path('update_event/<event_id>', views.update_event, name='update_event'),
+    path('delete_event/<event_id>', views.delete_event, name='delete_event'),
+    path('search_event/', views.search_event, name='search_event'),
 ]
