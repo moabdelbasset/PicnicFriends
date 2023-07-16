ef0e8166fb2f06f15dc363461c570c5e1f06b87b
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-07-16 19:50:35 +0000

    Set debug to false for deployment

--- Picnic_Friends/settings.py
+++ Picnic_Friends/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get("HOST"), '127.0.0.1', 'localhost', 'picnicfriends-b3d57c6873d2.herokuapp.com', 'picnicfriends.herokuapp.com', '8000-fitabigail-picnicfriend-xcna2nmep2p.ws-eu101.gitpod.io', '8000-szilviacser-picnicfrien-qjh7h4cr6kg.ws-eu101.gitpod.io',]
 
